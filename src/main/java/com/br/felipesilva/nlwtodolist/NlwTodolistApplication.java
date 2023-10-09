6f38863103af54a29c3f08682771d433d98e91da
Felipe Silva <felipe726silva@gmail.com>
2023-10-09 15:26:01 -0300

    create userController

---
+++ src/main/java/com/br/felipesilva/nlwtodolist/NlwTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.br.felipesilva.nlwtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/felipesilva/nlwtodolist/NlwTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.felipesilva.nlwtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
