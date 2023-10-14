0b03a76ec02a6797041c154a7992d5713f9d9e75
Felipe Silva <felipe726silva@gmail.com>
2023-10-14 16:40:19 -0300

    create subscription, get task list for user

--- src/main/java/com/br/felipesilva/nlwtodolist/repository/ITaskRepository.java
+++ src/main/java/com/br/felipesilva/nlwtodolist/repository/ITaskRepository.java
@@ -3,7 +3,9 @@ package com.br.felipesilva.nlwtodolist.repository;
 import com.br.felipesilva.nlwtodolist.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
